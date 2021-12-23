2535d11c824810328e3e75b904d507779f78415a
iKAN21 <88995178+iKAN21@users.noreply.github.com>
2021-12-23 11:59:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -157,7 +157,13 @@ def event_handle(event,json_line):
             line_bot_api.reply_message(rtoken, replyObj)
        elif msg == "นอนยัง" : 
             replyObj = TextSendMessage(text="ยังโว๊ย)
-            line_bot_api.reply_message(rtoken, replyObj)                  
+            line_bot_api.reply_message(rtoken, replyObj) 
+       elif msg == "covid" :
+            url = "https://covid19.ddc.moph.go.th/api/Cases/today-cases-all"
+            response = requests.get(url)
+            response = response.json()
+            replyObj = TextSendMessage(text=str(response))
+            line_bot_api.reply_message(rtoken, replyObj)                           
        else :
             headers = request.headers
             json_headers = ({k:v for k, v in headers.items()})
